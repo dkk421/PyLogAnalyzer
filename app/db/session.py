fe4b82e763f23dc48cff21a5b29077120d4cd540
Daria <daria.kr421@gmail.com>
2026-04-30 00:41:44 +0300

    Normal SessionLocal

--- app/db/session.py
+++ app/db/session.py
@@ -3,5 +3,16 @@ from sqlalchemy.orm import sessionmaker
 
 DATABASE_URL = "sqlite:///./logs.db"
 
-engine = create_engine(DATABASE_URL, echo=True)
-SessionLocal = sessionmaker(bind=engine)
+connect_args = {"check_same_thread": False} if DATABASE_URL.startswith("sqlite") else {}
+
+engine = create_engine(
+    DATABASE_URL,
+    connect_args=connect_args,
+    echo=False,
+)
+
+SessionLocal = sessionmaker(
+    autocommit=False,
+    autoflush=False,
+    bind=engine,
+)
